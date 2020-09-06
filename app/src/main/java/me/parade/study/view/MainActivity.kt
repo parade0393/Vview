4d0272eb13c35999f25ac1a39d947efad3de4497
parade岁月 <13703937803@163.com>
2020-09-07 07:29:48 +0800

    Initial commit 自定义view的学习过程，希望可以坚持下来

---
+++ app/src/main/java/me/parade/study/view/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.parade.study.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
